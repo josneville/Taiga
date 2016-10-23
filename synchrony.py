417e2fabf0cbcd2923ef9549116bf1713b84ed59
Rishant Dwivedi <Rishant Dwivedi>
2016-10-22 19:14:09 -0700

    works cats

--- synchrony.py
+++ synchrony.py
@@ -1,12 +1,21 @@
 import requests
 import json
+import random
 
 def get_next_purchase_item():
 	headers = {'username': 'giveitatry', 'password': 'Sh0wT!me', 'Content-Type': 'application/json'}
-	data = {"accountNum": "100009"}
-
+	randacct = random.randrange(100000, 105000)
+	data = {"accountNum": str(randacct)}
 	r = requests.post('https://syf2020.syfwebservices.com/syf/nextMostLikelyPurchase', headers=headers, data = json.dumps(data))
-	return r.text
+	resp = r.json()
+	categories = resp['categories']
+	maxcat = 0
+	maxelem = {}
+	for i in range(categories):
+		if categories[i]['probability'] > maxcat:
+			maxcat = categories[i]['probability']
+			maxelem = categories[i]
+	return maxelem
 
 if __name__ == "__main__":
 	print(get_next_purchase_item())
